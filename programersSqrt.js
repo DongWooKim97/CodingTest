ae9213d25620ecf074839e126710341647d0a54a
bxxloob_-@naver.com <nanaukim@naver.com>
2022-01-27 17:32:34 +0900

    프로그래머스 1단계 정수 제곱근 판별

--- programersSqrt.js
+++ programersSqrt.js
@@ -1,13 +1,23 @@
+// 1st Try
+// function solution(n) {
+//     var dap = 0;
+//     var x = Math.sqrt(n);
+//     console.log(x);
+//     for (let i = 0; i <= x + 1; i++) {
+//         if (i === x) {
+//             return (i + 1) * (i + 1);
+//         }
+//     }
+//     return -1
+
+// }
+// console.log(solution(3));
+
+/////2nd Try
 function solution(n) {
-    var dap = 0;
-    var x = Math.sqrt(n);
-    console.log(x);
-    for (let i = 0; i <= x + 1; i++) {
-        if (i === x) {
-            return (i + 1) * (i + 1);
-        }
+    for (let i = 0; i <= Math.sqrt(n); i++) {
+        if (i === Math.sqrt(n)) return (i + 1) ** 2;
     }
     return -1
-
 }
-console.log(solution(3));
+console.log(solution(16));
