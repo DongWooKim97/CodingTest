f5102e994a181564997876430b2406f6d35d05b4
bxxloob_-@naver.com <nanaukim@naver.com>
2022-02-14 17:34:23 +0900

    프로그래머스 1단계 정수의 약수와 덧셈

--- PG_LEVEL1_Q7.js
+++ PG_LEVEL1_Q7.js
@@ -1,26 +1,15 @@
-function divisors(num) {
-  let count = 0;
-
-  for (let i = 1; i <= num; i++) {
-    if (num % i == 0) {
-      count++;
-    }
-  }
-
-  return count;
-}
-
 function solution(left, right) {
-  let answer = 0;
-
-  for (let i = left; i <= right; i++) {
-    let count = divisors(i);
-    if (count % 2 === 0) {
-      answer += i;
-    } else {
-      answer -= i;
+  var sum = 0;
+  for (let a = left; a <= right; a++) {
+    var count = 0;
+    for (let b = 1; b <= a; b++) {
+      if (a % b === 0) {
+        count++;
+      }
     }
+    (count % 2 === 0) ? sum += a : sum -= a;
   }
-
-  return answer;
+  return sum;
 }
+
+console.log(solution(13, 17));
